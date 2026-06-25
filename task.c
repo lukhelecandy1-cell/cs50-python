afab0dc21af185e2e4349ed30e57f92278615d7f
candy lukhele <lukhelecandy1@gmail.com>
2026-06-25 16:38:25 +0200

    Add files via upload

---
+++ task.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+{
+    string name = "message";
+    printf("%c\n", name[0]);
+    printf("%c\n", name[2]);
+    printf("%c\n", name[6]);
+}
